588541c77db1255387e71f35fd015d0aade0cb1a
keveny <92321571+KevenyLima@users.noreply.github.com>
2022-03-27 15:24:43 -0300

    update app.js

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ import RoutesPost from './routes/RoutesPost'
 import RoutesCreator from './routes/RoutesCreator'
 const app = express()
 
-
+//config express
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -15,7 +15,9 @@ app.use(express.json())
 //routes
 app.use('/post',RoutesPost)
 app.use('/creator',RoutesCreator)
-
+app.get("/teste",(req,res)=>{
+    res.sendFile(__dirname+"")
+})
 const port = config.get('port')
 app.listen(port,async()=>{
     await db()
